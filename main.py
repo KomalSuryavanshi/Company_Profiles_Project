fb511472e39c8e01f097b30bd0242711874f2fc3
Komal <komalwork11@gmail.com>
2023-04-12 11:51:01 +0530

    Final Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas
+st.set_page_config(layout="wide")
+st.title("The Best Company")
+content = """""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+ ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+ esse cillum dolore eu fugiat nulla pariatur. 
+ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+  id est laborum."
+"""
+st.write(content)
+st.header("Our Team")
+col1, col2, col3 = st.columns(3)
+
+
+df = pandas.read_csv("data (1).csv", sep=",")
+with col1:
+    for index, row in df[:4].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images (1)/" + row["image"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images (1)/" + row["image"])
+
+with col3:
+    for index, row in df[8:12].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images (1)/" + row["image"])
+
+
